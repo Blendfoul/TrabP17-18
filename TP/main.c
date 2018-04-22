f55bdf87042ade13f95c05bac9a4f7a00be350a1
Pedro Lanhoso <pelan05@gmail.com>
2018-04-22 15:28:12 +0100

    v0.501
    minor

--- TP/main.c
+++ TP/main.c
@@ -1,14 +1,19 @@
 #include "header.h"
+#include <string.h>
+
+
+guitarra* criaVetor(int *tam){
+
+}
+
 
 int main(int argc, char** argv) {
 
 guitarra *stock;
 node *no;
-    
-    
-    
-    
-    
+
+
+
 return (EXIT_SUCCESS);
 }
 
